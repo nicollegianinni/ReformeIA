8622e4ab291e54893dd4db6ec3460d97de234f4e
NICHOLLE GIANINNI <gianinni.nicolle@gmail.com>
2025-08-04 10:11:09 -0300

    criada model

---
+++ mvc/src/main/java/com/project/mvc/domain/service/model/Medidas.java
@@ -0,0 +1,29 @@
+package com.project.mvc.domain.service.model;
+
+public class Medidas {
+
+    private double altura;
+    private double largura;
+
+    public Medidas(double altura, double largura) {
+        this.altura = altura;
+        this.largura = largura;
+    }
+
+    public double getLargura() {
+        return largura;
+    }
+
+    public void setLargura(double largura) {
+        this.largura = largura;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+}
